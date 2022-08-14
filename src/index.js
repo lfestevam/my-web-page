31a530909f1fa828c72283a17176fbe0e9431a1f
Luís Estevam <luisfestevam01@gmail.com>
2022-08-14 15:44:03 -0300

    responsividade semi-pronta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 alert (`
     ----------------ATENÇÃO---------------
-    O site ainda está em desenvolvimento;`)
+    O site ainda está em desenvolvimento;`);
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function menuMobile (){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle("active");
+}
+btnMobile.addEventListener('click', menuMobile);
+
+
+
