ae2cbd2afa9d105207fa6e063c213c7bf402a9b8
Luís Estevam <luisfestevam01@gmail.com>
2022-08-15 00:09:44 -0300

    resposividade de projetos, menu icon

--- src/index.js
+++ src/index.js
@@ -9,6 +9,36 @@ function menuMobile (){
     nav.classList.toggle("active");
 }
 btnMobile.addEventListener('click', menuMobile);
+//
+const inicio = document.getElementById('init');
 
+function inicioRemove(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle("active")
+}
+inicio.addEventListener('click', inicioRemove);
+//
+const sobre = document.getElementById('sobre');
 
+function sobreRemove(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle("active")
+}
 
+sobre.addEventListener('click', sobreRemove)
+//
+const tec = document.getElementById('tecno');
+
+function tecRemove(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle("active")
+}
+tec.addEventListener('click', tecRemove)
+//
+const projects = document.getElementById('projectsr');
+
+function projectRemove(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle("active")
+}
+projects.addEventListener('click', projectRemove)
